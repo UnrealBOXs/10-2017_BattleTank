1b6779446020894042e0c0f794d96ea03d20b412
wuttipat <wuttipatinbox@gmail.com>
2017-10-24 23:59:32 -0700

    addmytank.class and  tank controller class

---
+++ BattleTank/Source/BattleTank/Private/MyTank.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "MyTank.h"
+
+
+// Sets default values
+AMyTank::AMyTank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyTank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyTank::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AMyTank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
